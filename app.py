d5bd53a0793348dd87ab1ed692d37b16cce2a68e
Gaetan l'empereur du javashit <gaetann0211@gmail.com>
2020-03-04 16:02:33 +0100

    Add firebase listener and move logic to app.py

--- app.py
+++ app.py
@@ -2,24 +2,72 @@ from flask import Flask
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import RPi.GPIO as GPIO
 
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-questions = db.collection(u'questions').stream()
-for question in questions:
-    print(u'{} => {}'.format(question.id, question.to_dict()))
-
 app = Flask(__name__)
 
-@app.route('/')
+GPIO.setmode(GPIO.BCM)
 
-def index():
+pullupIndex = 25
+ledRows = [20, 21, 22, 23, 24]
+ledIndexes = [4, 5, 6, 12, 13]
 
- return 'Hello world'
+# Setup defaults
+GPIO.setup(pullupIndex, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for item in ledRows:
+	GPIO.setup(item, GPIO.OUT, initial=GPIO.HIGH)
+for item in ledIndexes:
+	GPIO.setup(item, GPIO.OUT, initial=GPIO.LOW)
 
-if __name__ == '__main__':
+def sendLeds(ledRow, ledIndex, callback):
+	ledRow0Indexed = ledRow - 1
+	ledIndex0Indexed = ledIndex - 1
 
- app.run(debug=True, host='0.0.0.0')
+	# Handle needed row
+	GPIO.output(ledRows[ledRow0Indexed], GPIO.LOW)
+
+	# Handle needed index
+	GPIO.output(ledIndexes[ledIndex0Indexed], GPIO.HIGH)
+
+	detectRotation(callback)
+
+
+def resetLeds():
+	# Handle rows
+	for item in ledRows:
+		 GPIO.output(item, GPIO.HIGH)
+
+	# Handle indexes
+	for item in ledIndexes:
+		 GPIO.output(item, GPIO.LOW)
+
+
+def detectRotation(callback):
+	isRotating = True if GPIO.input(pullupIndex) == 0 else False
+	while isRotating:
+		if(GPIO.input(pullupIndex) == 1):
+			resetLeds()
+			callback()
+			isRotating = False
+
+
+# Create a callback on_snapshot function to capture changes
+def on_snapshot(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        print(u'Received document snapshot: {}'.format(doc.id))
+        sendLeds(3, 2, lambda : db.collection(u'games').document(doc.id).update({u'isTransmitted': True}))
+
+doc_ref = db.collection(u'games').where(u'isEnded', u'==', True).where(u'isTransmitted', u'==', False)
+
+
+# Watch the document
+doc_watch = doc_ref.on_snapshot(on_snapshot)
+
+app.run(debug=True, host='0.0.0.0')
+
+
