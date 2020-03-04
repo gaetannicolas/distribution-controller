a042c137fe0dd6cd7d2272e40d38003964788680
Gaetan l'empereur du javashit <gaetann0211@gmail.com>
2020-03-04 12:15:42 +0100

    Implement logic rows/indexes

--- send-physical-signal.py
+++ send-physical-signal.py
@@ -1,10 +1,49 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-while True: # Run forever
- GPIO.output(4, GPIO.HIGH) # Turn on
- sleep(1) # Sleep for 1 second
- GPIO.output(4, GPIO.LOW) # Turn off
- sleep(1) # Sleep for 1 second
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+pullupIndex = 25
+ledRows = [20, 21, 22, 23, 24]
+ledIndexes = [4, 5, 6, 12, 13]
+
+# Setup defaults
+GPIO.setup(pullupIndex, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for item in ledRows:
+	GPIO.setup(item, GPIO.OUT, initial=GPIO.HIGH)
+for item in ledIndexes:
+	GPIO.setup(item, GPIO.OUT, initial=GPIO.LOW)
+
+
+def sendLeds(ledRow, ledIndex):
+	ledRow0Indexed = ledRow - 1
+	ledIndex0Indexed = ledIndex - 1
+
+	# Handle needed row
+	GPIO.output(ledRows[ledRow0Indexed], GPIO.LOW)
+
+	# Handle needed index
+	GPIO.output(ledIndexes[ledIndex0Indexed], GPIO.HIGH)
+
+	detectRotation()
+
+
+def resetLeds():
+	# Handle rows
+	for item in ledRows:
+		 GPIO.output(item, GPIO.HIGH)
+
+	# Handle indexes
+	for item in ledIndexes:
+		 GPIO.output(item, GPIO.LOW)
+
+
+def detectRotation():
+	isRotating = True if GPIO.input(pullupIndex) == 0 else False
+	while isRotating:
+		if(GPIO.input(pullupIndex) == 1):
+			resetLeds()
+			isRotating = False
+
+
+#sendLeds(3, 2)
+resetLeds()
