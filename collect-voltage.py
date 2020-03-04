5c142dd47627174334cf5cd1925543ae906c2256
Gaetan l'empereur du javashit <gaetann0211@gmail.com>
2020-03-04 16:05:02 +0100

    remove useless files

--- collect-voltage.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-from time import sleep # Import the sleep function from the time module
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-while True: # Run forever
- print(GPIO.input(17)) # Turn on
- sleep(1) # Sleep for 1 second
